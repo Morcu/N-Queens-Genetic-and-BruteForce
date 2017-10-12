560c50a1b5baaca9eeab05bb6ba338fe350e48cd
David Morcuende Cantador <david.morcuendecantador@telefonica.com>
2017-10-12 11:09:02 +0200

    finished bruteforce with backtraking

--- Brute Force/n-queensBF.py
+++ Brute Force/n-queensBF.py
@@ -1,80 +1,71 @@
-n = 4
+import copy
+
+n = 10
 global Matrix
 q = 9
 
 def main():
-    print "Hello, world!"
-    queens = n -1
-    startX = 0
-    startY = 0
-    Matrix = [[0 for x in range(n)] for y in range(n)] 
+    #Genera la matriz de N x N
+    global Matrix 
+    Matrix= [[0 for x in range(n)] for y in range(n)] 
+    #Matriz que contendra todos los resultados
+    global Result
     Result = []
 
-    while(queens != 0):
-        print(queens)
-        posx, posy = findPlace(0, 0, 0, Matrix)
-        setQueen(posx, posy, Matrix)
-        for i in range(len(Matrix[x])):
-            print(Matrix[i])
-        print('----------')
-        queens = queens - 1
-        for check in range(len(Matrix[x])):
-            if (len(set(Matrix[check])) <= 1 and Matrix[check][0] == 1):
-                print('nope')
-                if(startY == n-1):
-                    startX = startX + 1
-                    startY = 0
-                else:
-                    startY = startY + 1
-                Matrix = [[0 for x in range(n)] for y in range(n)]
-                posx, posy = findPlace(1,startX ,startY, Matrix)
-                setQueen(posx, posy, Matrix)
-                for i in range(len(Matrix[x])):
-                    print(Matrix[i])
-                print('----------')
-                queens = n -1
+    recursive_solver(Matrix, 0)
+    print('FIN')
+    print(len(Result))
     
-
+#Se comprueba si puede ponerse o no la reina
 def setQueen(x, y, Matrix):
-    print(x, y)
-    Matrix[x][y] = q
+    
     #horizontal
     for i in range(len(Matrix[x])):
-        if(Matrix[x][i] == 0):
-            Matrix[x][i] = 1
+        if(Matrix[x][i] == 1):
+            return False
     #vertical
     for j in range(len(Matrix[x])):
-        if(Matrix[j][y] == 0):
-            Matrix[j][y] = 1
+        if(Matrix[j][y] == 1):
+            return False
 
-    #Revisar diagonales desde la reina hacia arriba
     #Diagonal principal       
     for k in range(len(Matrix[x])):
         if (x+k < n and y+k < n):
-            if(Matrix[x+k][y+k] == 0):
-                Matrix[x+k][y+k] = 1
+            if(Matrix[x+k][y+k] == 1):
+                return False
         if (x-k > -1 and y-k > -1):
-            if(Matrix[x-k][y-k] == 0):
-                Matrix[x-k][y-k] = 1
+            if(Matrix[x-k][y-k] == 1):
+                return False
    
     #Diagonal Inversa        
     for l in range(len(Matrix[x])):
         
         if (x+l < n and y-l >= 0):
-            if(Matrix[x+l][y-l] == 0):
-                Matrix[x+l][y-l] = 1
+            if(Matrix[x+l][y-l] == 1):
+                return False
         if (x-l > -1 and y+l < n):
-            if(Matrix[x-l][y+l] == 0):
-                Matrix[x-l][y+l] = 1
+            if(Matrix[x-l][y+l] == 1):
+                return False
+    return True
+
+def recursive_solver(matrix, col):
     
-def findPlace(start, startX, startY, Matrix):
-    if(start == 0):
-        for x in range(n):
-            for y in range(n):
-                if(Matrix[x][y] == 0):
-                    return x, y
-    else:
-        return startX, startY
+    n = len(matrix[0])
+
+    if (col >= n):
+        return
+    for x in range(n):
+        if setQueen(x, col, matrix):
+            matrix[x][col] = 1
+            if col == n - 1:
+                solucion = saved_board = copy.deepcopy(matrix)
+                Result.append(solucion)
+                matrix[x][col] = 0
+                return
+            recursive_solver(matrix, col + 1)
+            
+            #Si hay que aplicar backtracking, se puede aplicar poniendo esa reina a 0 y siguiendo con el algoritmo
+            matrix[x][col] = 0
 
 if __name__ == '__main__':
     main()
